e4cfeddb8bf3a12a56bda4fb47b0a3288f7fb974
dabug <superparisian@gmail.com>
2018-07-06 12:22:19 +0200

    Ajouté les fichiers des programmes Hello et Personne

---
+++ Personne.java
@@ -0,0 +1,26 @@
+import java.util.Date;
+public class Personne{
+  String nom, prenom;
+  Date dateNaissance;
+  public Personne(String n, String p, Date d){
+    nom=n;
+    prenom=p;
+    dateNaissance=d;
+  }
+  public boolean equals(Personne autre){
+    boolean res;
+    res = nom.equals("autre.nom");
+    res = res && (prenom.equals(autre.prenom));
+    res = res && (dateNaissance.equals(autre.dateNaissance));
+    return res;
+  }
+  public String getNom(){
+    return nom;
+  }
+  public Date getDateNaissance(){
+    return dateNaissance;
+  }
+  public String toString(){
+    return nom + " " + prenom + " " + dateNaissance;
+  }
+}
